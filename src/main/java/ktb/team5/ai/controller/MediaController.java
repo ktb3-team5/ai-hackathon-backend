a09c5d4c411faf8174605de27959d3d2bcca99a9
som0309 <eunseolim1018@naver.com>
2025-12-17 19:25:22 +0900

    feat: 인기 미디어 top 10 목록 조회 구현

--- src/main/java/ktb/team5/ai/controller/MediaController.java
+++ src/main/java/ktb/team5/ai/controller/MediaController.java
@@ -1,11 +1,23 @@
 package ktb.team5.ai.controller;
 
+import ktb.team5.ai.dto.MediaTopResponse;
+import ktb.team5.ai.service.MediaService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/media")
 @AllArgsConstructor
 public class MediaController {
+
+    private final MediaService mediaService;
+
+    @GetMapping("/top10")
+    public List<MediaTopResponse> getTop10() {
+        return mediaService.getTop10Media();
+    }
 }
