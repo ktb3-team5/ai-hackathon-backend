7feb211c0825c7ea54c270b01a0041ba20080dfe
jian.lee(이가연) <qaz102912@gmail.com>
2025-12-17 16:06:33 +0900

    Initial Commit

---
+++ src/test/java/ktb/team5/ai/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package ktb.team5.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
